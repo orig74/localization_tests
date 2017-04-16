9b2720524861400173ef4ad865fcb8c8809f7edc
ori <ori.ganoni@gmail.com>
2017-04-16 14:06:37 +1200

    need to got to ipython notbook....

--- camerasim.py
+++ camerasim.py
@@ -46,9 +46,46 @@ def manuver2():
             if ind==300:
                 step_x=0
                 step_y=-0.01
-            vec[0]+=step_x
+            vec[0]-=step_x
             vec[1]+=step_y
             
+            if rot_pitch>0:
+                pitch_sign=1 if (ind%rot_pitch)<(rot_pitch//2) else -1
+                vec[3]+=pitch_sign*0.1/180.0*np.pi
+            if rot_roll>0:
+                roll_sign=1 if (ind%rot_roll)<(rot_roll//2) else -1
+                vec[4]+=roll_sign*0.1/180.0*np.pi
+            yield vec
+
+def manuver3():
+    #x,y,z,rx,ry,rz
+    vec=np.zeros(6)
+    vec[2]=3
+    #vec[3]=-90/180.0*np.pi
+    for _ in range(50):
+        vec[2]+=0.1
+        yield vec
+
+    for rot_pitch,rot_roll in [(0,0)]: 
+        for ind in range(400):
+            if ind==0: 
+                step_x=-0.01
+                step_y=0
+            if ind==100:
+                step_x=0
+                step_y=0.01
+            if ind==200:
+                step_x=0.01
+                step_y=0
+            if ind==300:
+                step_x=0
+                step_y=-0.01
+            vec[0]-=step_x
+            vec[1]+=step_y
+            yield vec
+
+    for rot_pitch,rot_roll in [(0,30)]:             
+        for ind in range(400):
             if rot_pitch>0:
                 pitch_sign=1 if (ind%rot_pitch)<(rot_pitch//2) else -1
                 vec[3]+=pitch_sign*0.1/180.0*np.pi
@@ -60,11 +97,12 @@ def manuver2():
 
 
 
+
 class Capture(object):
     def __init__(self,camera_matrix,size,noise_model=None):
         self.K=mat(camera_matrix).reshape(3,3)
         self.size=size
-        self.manuever=manuver2()
+        self.manuever=manuver3()
         self.last_points=None
         self.last_position=None
 
@@ -75,15 +113,23 @@ class Capture(object):
             return False,None
         x,y,z,rx,ry,rz=self.last_position
         img=np.zeros((self.size[0],self.size[1],3),dtype='uint8')
-        C=np.array([x,y,z])
+        C=-np.array([x,y,z])
         #0=RC+T T=-R.T*C
         
         R=utils.eulerAnglesToRotationMatrix([rx,ry,rz]) 
-        T=-mat(R).T*mat(C).T
-        pts=(self.K*((mat(R)*generate_3d_points().T).T+T.T).T).T
-        pts/=pts[:,2]
+        
+        if 0:
+            T=-mat(R).T*mat(C).T
+            pts=(self.K*((mat(R).T*generate_3d_points().T).T+T.T).T).T
+            pts/=pts[:,2]
+        else:
+            R_vec,_=cv2.Rodrigues(R)
+            pts,jac=cv2.projectPoints(generate_3d_points(),R_vec,-C,self.K,np.zeros(5))
+            pts=pts.reshape(-1,2)
+ 
         for ptm in pts:
-            pt=list(map(int,ptm.A1)) 
+            #pt=list(map(int,ptm.A1)) 
+            pt=list(map(int,ptm))
             cv2.rectangle(img,(pt[0]-1,pt[1]-1),(pt[0]+1,pt[1]+1),(0,255,0),1)
         self.last_points=pts[:,:2]
         return True,img
